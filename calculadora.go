7b64ba3300d47846082e8625bd58352ac6fe0b00
jromero1502 <romerojulian115@gmail.com>
2021-03-24 14:30:29 -0500

    Initial commit

---
+++ calculadora.go
@@ -0,0 +1,41 @@
+package mycalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func (calc) operate(entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+	switch operador {
+	case "+":
+		return operador1 + operador2
+	case "-":
+		return operador1 - operador2
+	case "*":
+		return operador1 * operador2
+	case "/":
+		return operador1 / operador2
+	default:
+		fmt.Println("Operador no soportado")
+		return 0
+	}
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jromero1502/mycalculator
+
+go 1.16
